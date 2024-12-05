db776de71b76a4fe7811ac27f4430af49f37468f
khristine-g <githigekhristine@gmail.com>
2024-12-05 16:59:43 +0300

    Fixing cors issues

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,11 +1,21 @@
-# config/initializers/cors.rb
+# # config/initializers/cors.rb
 
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins origins 'https://truebeauty-ashen.vercel.app'
+#     # Replace with your React frontend's URL
+#     resource '*', 
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
+# end
+
+
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://truebeauty-ashen.vercel.app/ '  # Replace with your React frontend's URL
-    resource '*', 
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    origins 'https://truebeauty-ashen.vercel.app'  # Replace with your frontend's domain
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
